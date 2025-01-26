d2dadfc2848f1e6d6dd91f2c1521114007ac8b60
Ben Shutt <shuttbenjamin@gmail.com>
2025-01-26 10:56:19 +0000

    Add SwiftTime

---
+++ SwiftTime.playground/Contents.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+// MARK: - Constants
+
+let numbers = Array(0..<100_000)
+let numberOfTests = 10_000
+let numberOfTestsF = TimeInterval(numberOfTests)
+
+// MARK: - Algorithms
+
+func forDouble(_ numbers: [Int]) -> [Int] {
+    var doubles = [Int]()
+    for number in numbers {
+        doubles.append(number * 2)
+    }
+    return doubles
+}
+
+func forEnumeratedDouble(_ numbers: [Int]) -> [Int] {
+    var doubles = [Int](repeating: 0, count: numbers.count)
+    for (i, number) in numbers.enumerated() {
+        doubles[i] = number * 2
+    }
+    return doubles
+}
+
+func mapDouble(_ numbers: [Int]) -> [Int] {
+    numbers.map { $0 * 2 }
+}
+
+// MARK: - Helper
+
+func test(
+    _ numbers: [Int],
+    operation: ([Int]) -> [Int],
+    name: String
+) {
+    var average: Duration = .zero
+    (0..<numberOfTests).forEach { _ in
+        let time = ContinuousClock().measure { _ = operation(numbers) }
+        average += time / numberOfTestsF
+    }
+    print("\(name) \(average.formattedMicroSeconds)")
+}
+
+extension ContinuousClock.Duration {
+    var formattedMicroSeconds: String {
+        formatted(.units(
+            allowed: [.microseconds],
+            maximumUnitCount: 1
+        ))
+    }
+}
+
+// MARK: - Main
+
+try await Task.sleep(for: .seconds(1))
+test(numbers, operation: forDouble, name: "forDouble")
+test(numbers, operation: forEnumeratedDouble, name: "forEnumeratedDouble")
+test(numbers, operation: mapDouble, name: "mapDouble")
---
+++ SwiftTime.playground/Resources/README.md
@@ -0,0 +1,10 @@
+### SwiftTime
+
+Should be run as a SwiftPM executable with the following script with optimization:
+
+```bash
+swift run \
+  -Xswiftc -O \
+  --configuration release \
+    SwiftTime
+```
